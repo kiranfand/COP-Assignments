e32851ba54f93b5eae119a7c42be28c774345873
kiranfand <105260190+kiranfand@users.noreply.github.com>
2023-04-26 22:31:16 +0530

    Add files via upload

---
+++ DateClass.java
@@ -0,0 +1,8 @@
+
+public class DateClass {
+	public int day, month, year;
+
+	public void display() {
+		System.out.println(day + "/" + month + "/" + year);
+	}
+}
---
+++ date.java
@@ -0,0 +1,13 @@
+
+public class date {
+
+	public static void main(String[] args) {
+		DateClass obj1 = new DateClass();
+		DateClass obj2 = new DateClass();
+
+		obj1.display();
+		obj2.display();
+
+	}
+
+}
